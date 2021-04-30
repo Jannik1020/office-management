888a170cdbe6d8cc19272ba9fbe9b1b2c1d90bd6
Jannik1020 <jannik.thiele.official@gmail.com>
2021-04-30 20:11:41 +0200

    styled no-tasks text

--- src/features/tasks/Components/TaskView.js
+++ src/features/tasks/Components/TaskView.js
@@ -1,13 +1,19 @@
 import {useSelector} from "react-redux"
 import {selectTasks, selectCompletedTasks} from "../tasksSlice"
 import styles from "./TaskView.module.css"
+import {ImArrowDown} from "react-icons/im"
   
   export default function TaskView () { 
     var tasks = useSelector(selectTasks)
     var completedTask = useSelector(selectCompletedTasks)
     return (
       <div>
-        {(tasks.length === 0 && completedTask.length === 0) && <div className={styles.defaultText}>Füge eine Aufgabe hinzu!</div>}        
+        {(tasks.length === 0 && completedTask.length === 0) && (
+          <div className={styles.defaultText}>
+            Füge eine Aufgabe hinzu!
+            <ImArrowDown className={styles.defaultIcon}></ImArrowDown>
+          </div>
+        )}        
         {(tasks.length === 0 && completedTask.length > 0) && <div className={styles.completedText}>Super! Du hast alle Aufgaben erledigt</div>}
         <ul className={styles.taskList}>
           {tasks}
