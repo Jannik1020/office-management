ea52ecf77f775ad0fcd98165c763a78d7b9f94d5
Jannik1020 <jannik.thiele.official@gmail.com>
2021-04-30 12:18:00 +0200

    move TaskView logic from local state to redux store

--- src/features/tasks/Components/TaskView.js
+++ src/features/tasks/Components/TaskView.js
@@ -1,44 +1,19 @@
 import React, {useEffect, useRef, useState} from "react"
+import {nanoid} from "@reduxjs/toolkit"
 import Task from "./Task.js"
 import styles from "./TaskView.module.css"
   
   export default function TaskView () {
-
-    const createTask = (index, title, callback, checked) => <Task key={index} id={index} title={title} callback={callback} checked={checked}/>
+    //nanoid: random Id
+    const createTask = (title, callback, checked) => <Task key={nanoid()} id={nanoid()} title={title} callback={callback} checked={checked}/>
   
     const handleCheckboxClick = (doneTask) => { // doneTask=[checked, id]
-      var changedTask;
-      var taskIndex;
-
-      var newTasks = tasks;
-      var newTasksDone = tasksDone;        
-
       if(doneTask.checked){
-        
-        changedTask = tasks.find(cTask => cTask.props.id === doneTask.id);
-        taskIndex = tasks.findIndex(cTask => cTask.props.id === doneTask.id);
-
-        newTasks.splice(taskIndex,1);
-       
-        changedTask = createTask(changedTask.props.id, changedTask.props.title, handleCheckboxClick, true)//<Task key={task.props.id} id={task.props.id} title={task.props.title} callback={this.handleCheckboxClick} checked={true}/>
-        newTasksDone.unshift(changedTask)
-  
+                
       } else {
-        
-
-        changedTask = tasksDone.find(cTask => cTask.props.id === doneTask.id);
-        taskIndex = tasksDone.findIndex(cTask => cTask.props.id === doneTask.id);
-        
-        newTasksDone.splice(taskIndex,1);
-  
-        changedTask = createTask(changedTask.props.id, changedTask.props.title, handleCheckboxClick, false)
-        newTasks.push(changedTask)
-        
+      
         
       }
-
-      changeTasks([...newTasks]);
-      changeTasksDone((newTasksDone !== undefined ? [...newTasksDone]: []));
     }
 
     const [tasks, changeTasks] = useState(["Brötchen backen", "Brötchen verkaufen", "Geld aus der Kasse nehmen", "Feierabend"].map((taskTitle, index) => {
--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,19 +1,27 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = {tasks: [], completedTasks: []};
 
 export const tasksSlice = createSlice({
   name:"tasks",
   initialState,
   reducers: {
-    addTask: (state, action) => {
-      state.push(action.payload);
+    addTask: (state, action) => { // action.payload: <Task ... />
+      state.unshift(action.payload);
     },
-    completeTask: (state, action) => {
+    completeTask: (state, action) => { //action.payload: id (checked)
+      const id = action.payload;
+      const completedTask = state.tasks.filter((task) => task.props.id === id);      
       
+      state.tasks = state.tasks.filter((task) => task.props.id !== id);
+      state.completedTasks = state.completedTasks.unshift(completedTask) 
     },
-    readdTask: (state, action) => {
-
+    readdTask: (state, action) => { //action.payload: id (unchecked)
+      const id = action.payload;
+      const completedTask = state.tasks.filter((task) => task.props.id === id);      
+      
+      state.completedTasks = state.completedTasks.filter((task) => task.props.id !== id);
+      state.tasks = state.tasks.push(completedTask) 
     }
   }
 })
