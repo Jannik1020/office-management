ad44f1ecb1cbb28ced710703a1e5a8baefee8920
Jannik1020 <jannik.thiele.official@gmail.com>
2021-04-30 10:41:08 +0200

    removed AddViews inputs events

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -55,10 +55,10 @@ export default function AddView (props) {
     return (
         <div className="add-view">
             <div className="add-btn">
-                <BsPlus onClick={} className="ic-add-btn"/>
+                <BsPlus /*onClick={}*/ className="ic-add-btn"/>
             </div>
             <div className="add-field">
-                <input type="input" value={value} onChange={} placeholder="Neue Aufgabe" className="add-field-input" onKeyPress={}/>
+                <input type="input" /*value={value} onChange={}*/ placeholder="Neue Aufgabe" className="add-field-input" /*onKeyPress={}*//>
             </div>    
         </div>
     )
